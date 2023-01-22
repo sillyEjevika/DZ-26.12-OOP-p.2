261185102bdc9a8bf3de4bbca717dd254b41fe56
sillyEjevika <Doroteia_Vika@mail.ru>
2023-01-22 19:15:00 +0300

    ДЗ №2 ООП

--- src/Main.java
+++ src/Main.java
@@ -2,76 +2,18 @@ import transport.Car;
 
 public class Main {
     public static void main(String[] args) {
-        // Задача №1
+             // Задача №1
         System.out.println("Задача №1");
-
-        Car lada1 = new Car("Lada");
-        lada1.setModel("Granta");
-        lada1.setEngineVolume(1.7);
-        lada1.setColor("желтый");
-        lada1.setYear(2015);
-        lada1.setCountry("Россия");
-        System.out.println(lada1);
-
-        Car audi1 = new Car("Audi");
-        audi1.setModel("A8 50 L TDI quattro");
-        audi1.setEngineVolume(3.0);
-        audi1.setColor("черный");
-        audi1.setYear(2020);
-        audi1.setCountry("Германия");
-        System.out.println(audi1);
-
-        Car bmw1 = new Car("BMW");
-        bmw1.setModel("Z8");
-        bmw1.setEngineVolume(3.0);
-        bmw1.setColor("черный");
-        bmw1.setYear(2021);
-        bmw1.setCountry("Германия");
-        System.out.println(bmw1);
-
-        Car kia1 = new Car("Kia");
-        kia1.setModel("Sportage 4-го поколения");
-        kia1.setEngineVolume(2.4);
-        kia1.setColor("красный");
-        kia1.setYear(2018);
-        kia1.setCountry("Южная Корея");
-        System.out.println(kia1);
-
-        Car hyundai1 = new Car("Hyundai");
-        hyundai1.setModel("Avante");
-        hyundai1.setEngineVolume(1.6);
-        hyundai1.setColor("оранжевый");
-        hyundai1.setYear(2016);
-        hyundai1.setCountry("Южная Корея");
-        System.out.println(hyundai1);
-
-        // Задача №2
-        System.out.println("Задача №2");
-        Car lada = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия");
-        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия");
-        Car bmw = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия");
-        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея");
-        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016,"Южная Корея");
+        Car lada = new Car("Lada", "Granta", 1.7, "желтый", 2015, "Россия", "МКПП", "седан", "г123ма123rus", false, null);
+        Car audi = new Car("Audi", "A8 50 L TDI quattro", 3.0, "черный", 2020, "Германия", "МКПП", "седан", "в123ма123rus", true, new Car.Key(false, true));
+        Car bmw = new Car("BMW", "Z8", 3.0, "черный", 2021, "Германия","АКПП", "купе", "л333ма23rus", true, new Car.Key(true, true));
+        Car kia = new Car("Kia", "Sportage 4-го поколения", 2.4, "красный", 2018, "Южная Корея","АКПП", "хэчбэк", "н555ма03rus", false, null);
+        Car hyundai = new Car("Hyundai", "Avante", 1.6, "оранжевый", 2016,"Южная Корея"," ", " ", " ", false, new Car.Key(false, true));
 
         System.out.println(lada);
         System.out.println(audi);
         System.out.println(bmw);
         System.out.println(kia);
         System.out.println(hyundai);
-
-        // Задача №3
-        System.out.println("Задача №3");
-        Car lada2 = new Car("Lada", null, -2, "", -22, "Россия");
-        Car audi2 = new Car("Audi", " ", 0,  null, 0, "Германия");
-        Car bmw2 = new Car("BMW", null, -1.7, " ", -2021, "Германия");
-        Car kia2 = new Car("Kia", " ", -2.4, " ", 0, "Южная Корея");
-        Car hyundai2 = new Car("Hyundai", null, 0, null, -2016,"Южная Корея");
-
-        System.out.println(lada2);
-        System.out.println(audi2);
-        System.out.println(bmw2);
-        System.out.println(kia2);
-        System.out.println(hyundai2);
-    }
-
+        }
 }
--- transport/Car.java
+++ transport/Car.java
@@ -12,10 +12,32 @@ public class Car {
     private String regNumber;
     private int capacity;
     boolean summerTyres;
+    private Key key;
+
+    public static class Key {
+       private final boolean remoteEngineStart;
 
 
-    public Car(String brand) {
-        this.brand = brand;
+
+        private final boolean keylessEntry;
+
+        public Key(boolean remoteEngineStart, boolean keylessEntry) {
+            this.remoteEngineStart = remoteEngineStart;
+            this.keylessEntry = keylessEntry;
+        }
+        public boolean isRemoteEngineStart() {
+            return remoteEngineStart;
+        }
+
+        public boolean isKeylessEntry() {
+            return keylessEntry;
+        }
+
+        @Override
+        public String toString() {
+            return (remoteEngineStart ? "удаленный запуск двигателя" : "нет удалненного запуска двигателя") + ", " +
+                            (keylessEntry ? "безключевой досутп" : "нет безключевого доступа");
+        }
     }
 
     public String getBrand() {
@@ -73,29 +95,79 @@ public class Car {
         return summerTyres;
     }
 
-    public Car (String brand, String model, double engineVolume, String color, Integer year, String country ) {
+    public void setSummerTyres() {
+        this.summerTyres = summerTyres;
+    }
+    public Key getKey() {
+        return key;
+    }
+    public void setKey(Key key) {
+        if (key == null) {
+            key = new Key(false, false);
+        }
+        this.key = key;
+    }
+
+
+    public Car (String brand, String model, double engineVolume, String color, Integer year, String country, String transmission, String bodyType, String regNumber, boolean summerTyres, Key key) {
+        String model1;
         this.brand = brand;
-        this.model = model;
+        model1 = model;
         this.engineVolume = engineVolume;
         this.color = color;
         this.year = year;
         this.country = country;
-        if (model == null || model.isEmpty() || model.isBlank()) {
-            this.model = "default";
+        this.summerTyres = summerTyres;
+        if (model == null || model.isEmpty() || model == " ") {
+            model1 = "default";
         }
+        this.model = model1;
         if (engineVolume <= 0) {
             this.engineVolume = 1.5;
         }
-        if (color == null || color.isEmpty() || model.isBlank()) {
+        if (color == null || color.isEmpty() || model == " ") {
             this.color = "белый";
         }
         if (year <= 0) {
             this.year = 2000;
         }
+        if (transmission == null || transmission.isEmpty() || transmission == " ") {
+            this.transmission = "мкпп";
+        }
+        if (bodyType == null || bodyType.isEmpty() || bodyType == " ") {
+            this.bodyType = "хэчбэк";
+        }
+        if (regNumber == null || regNumber.isEmpty() || regNumber == " ") {
+            this.regNumber = "z000zz178rus";
+        }
+        if (capacity <=0){
+            this.capacity = 4;
+        }
+        setKey(key);
+    }
 
+    public void changeTyres(int month) {
+        if ((month >= 10 && month <= 12) || (month >= 1 && month <= 4)) {
+            summerTyres = false;
+        }
+        if (month >= 5 && month <= 9) {
+            summerTyres = true;
+        }
     }
     @Override
     public String toString() {
-        return "Автомобиль: " + "марка: " + brand + ", модель: " + model + ", объем двигателя: " + engineVolume + ", цвет: " + color + ", год выпуска: " + year + ", страна производства: " + country;
+        return "Автомобиль: " +
+                "марка: " + brand +
+                ", модель: " + model +
+                ", объем двигателя: " + engineVolume +
+                ", цвет: " + color +
+                ", год выпуска: " + year +
+                ", страна производства: " + country +
+                ", коробка передач: " + transmission +
+                ", тип кузова: " + bodyType +
+                ", регистрационный номер: " + regNumber +
+                ", количество мест: " + capacity +
+                ", резитна: " + (summerTyres?"летняя":"зимняя")+
+                ", " + key;
     }
 }
